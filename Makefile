de1f5e05fd791590a90b0144f5d89d526562ee3e
hellopradeep69 <hellopradeep69@gmail.com>
2026-01-20 19:55:26 +0530

    feat(Makefile): altered makefile and add gitignore

--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
 CC = gcc
 
-CFLAGS = -Wall -Wextra -lraylib
+CFLAGS = -Wall -Wextra -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
 
 run:
 	$(CC) main.c $(CFLAGS) -o main && ./main
 
 compile:
 	$(CC) main.c $(CFLAGS) -o main
+
+clean:
+	rm main
--- main.c
+++ main.c
@@ -1,13 +1,20 @@
 #include "raylib.h"
+#include <stdio.h>
 #include <stdlib.h>
 
+#define WIDTH 700
+#define HEIGHT 700
+#define BLOCK_SIZE 150
+
 int main(void) {
-  InitWindow(700, 700, "Lost");
+  InitWindow(WIDTH, HEIGHT, "Lost");
 
   while (!WindowShouldClose()) {
     BeginDrawing();
-    ClearBackground(RED);
-    DrawText("hello", 110, 100, 29, YELLOW);
+    ClearBackground(BLACK);
+    DrawText("Mr.Real", 110, 100, 29, RED);
+
+    DrawRectangle(WIDTH / 2, HEIGHT / 2, BLOCK_SIZE, BLOCK_SIZE, GREEN);
 
     if (IsKeyDown(KEY_Q)) {
       exit(0);
