8726360a2d790f2613562ad20e4982f600fa1ba7
k.pradeep <Hellopradeep69@gmail.com>
2026-02-21 16:37:29 +0530

    fix(usage): added usage for the cc

--- src/main.c
+++ src/main.c
@@ -31,8 +31,10 @@ void Load_image(int CurrentImage, char imageList[MAX][MAX_Path]) {
 
 int main(int argc, char **argv) {
 
-  if (argc < 2 || strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "-help") == 0) {
+  if (argc < 2 || strcmp(argv[1], "-h") == 0 ||
+      strcmp(argv[1], "--help") == 0) {
     printf("Usage:\n");
+    printf("%s <file-or-directory>\n", argv[0]);
     return 1;
   }
 
